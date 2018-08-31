d1960d26a5ddbb7d13eee07bcf6593120727bd24
xiphiasnonus <foster.pianist@gmail.com>
2018-08-31 10:39:33 -0400

    Initial Commit

---
+++ Snake/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Snake
+//
+//  Created by Thomas Foster on 8/31/18.
+//  Copyright © 2018 Thomas Foster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
